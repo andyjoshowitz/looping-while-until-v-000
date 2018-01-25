1c3f546c8ffde4b996b3d2cb5eb2b33b2b550425
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-01-25 17:17:30 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,6 @@
 def using_until
-  levitation_force = 0
-  until levititation_force = 4
+  levitation_force = 6
+  until levititation_force == 10
     puts "Wingardian Leviosa"
     levitation_force += 1
   end         
